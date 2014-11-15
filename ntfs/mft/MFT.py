affee771c0931b4b8d2d178a2cc7fd06c09ee330
Willi Ballenthin <willi.ballenthin@gmail.com>
2014-11-15 17:57:03 -0500

    mft: remove array.array usage, remove NTFSFile

--- ntfs/mft/MFT.py
+++ ntfs/mft/MFT.py
@@ -16,8 +16,6 @@ from ..BinaryParser import Nestable
 
 g_logger = logging.getLogger("ntfs.mft")
 
-# TODO: remove most references to array.array
-
 
 class INDXException(Exception):
     """
@@ -75,7 +73,7 @@ class FixupBlock(Block):
             self.pack_word(fixup_offset, new_value)
 
             check_value = self.unpack_word(fixup_offset)
-            logging.debug("Fixup verified at %s and patched from %s to %s.",
+            g_logger.debug("Fixup verified at %s and patched from %s to %s.",
                           hex(self.offset() + fixup_offset),
                           hex(fixup_value), hex(check_value))
 
@@ -370,7 +368,6 @@ def INDEX_ROOT(Block, Nestable):
 
 class NTATTR_STANDARD_INDEX_HEADER(Block):
     def __init__(self, buf, offset, parent):
-        logging.debug("INDEX NODE HEADER at %s.", hex(offset))
         super(NTATTR_STANDARD_INDEX_HEADER, self).__init__(buf, offset)
         self.declare_field("dword", "entry_list_start", 0x0)
         self.declare_field("dword", "entry_list_end")
@@ -420,7 +417,6 @@ class NTATTR_STANDARD_INDEX_HEADER(Block):
 
 class IndexRootHeader(Block):
     def __init__(self, buf, offset, parent):
-        logging.debug("INDEX ROOT HEADER at %s.", hex(offset))
         super(IndexRootHeader, self).__init__(buf, offset)
         self.declare_field("dword", "type", 0x0)
         self.declare_field("dword", "collation_rule")
@@ -439,7 +435,6 @@ class IndexRootHeader(Block):
 
 class IndexRecordHeader(FixupBlock):
     def __init__(self, buf, offset, parent):
-        logging.debug("INDEX RECORD HEADER at %s.",  hex(offset))
         super(IndexRecordHeader, self).__init__(buf, offset, parent)
         self.declare_field("dword", "magic", 0x0)
         self.declare_field("word",  "usa_offset")
@@ -490,7 +485,6 @@ class INDEX_ALLOCATION(FixupBlock):
 
 class IndexEntry(Block):
     def __init__(self, buf, offset, parent):
-        logging.debug("INDEX ENTRY at %s.", hex(offset))
         super(IndexEntry, self).__init__(buf, offset)
         self.declare_field("qword", "mft_reference", 0x0)
         self.declare_field("word", "length")
@@ -519,7 +513,6 @@ class StandardInformationFieldDoesNotExist(Exception):
 class StandardInformation(Block):
     # TODO(wb): implement sizing so we can make this nestable
     def __init__(self, buf, offset, parent):
-        logging.debug("STANDARD INFORMATION ATTRIBUTE at %s.", hex(offset))
         super(StandardInformation, self).__init__(buf, offset)
         self.declare_field("filetime", "created_time", 0x0)
         self.declare_field("filetime", "modified_time")
@@ -588,7 +581,6 @@ class StandardInformation(Block):
 
 class FilenameAttribute(Block, Nestable):
     def __init__(self, buf, offset, parent):
-        logging.debug("FILENAME ATTRIBUTE at %s.", hex(offset))
         super(FilenameAttribute, self).__init__(buf, offset)
         self.declare_field("qword", "mft_parent_reference", 0x0)
         self.declare_field("filetime", "created_time")
@@ -649,7 +641,6 @@ class SlackIndexEntry(IndexEntry):
 class Runentry(Block, Nestable):
     def __init__(self, buf, offset, parent):
         super(Runentry, self).__init__(buf, offset)
-        logging.debug("RUNENTRY @ %s.", hex(offset))
         self.declare_field("byte", "header")
         self._offset_length = self.header() >> 4
         self._length_length = self.header() & 0x0F
@@ -709,7 +700,6 @@ class Runentry(Block, Nestable):
 class Runlist(Block):
     def __init__(self, buf, offset, parent):
         super(Runlist, self).__init__(buf, offset)
-        logging.debug("RUNLIST @ %s.", hex(offset))
 
     @staticmethod
     def structure_size(buf, offset, parent):
@@ -799,7 +789,6 @@ class Attribute(Block, Nestable):
 
     def __init__(self, buf, offset, parent):
         super(Attribute, self).__init__(buf, offset)
-        logging.debug("ATTRIBUTE @ %s.", hex(offset))
         self.declare_field("dword", "type")
         self.declare_field("dword", "size")  # this value must rounded up to 0x8 byte alignment
         self.declare_field("byte", "non_resident")
@@ -873,7 +862,6 @@ class MFTRecord(FixupBlock):
     """
     def __init__(self, buf, offset, parent, inode=None):
         super(MFTRecord, self).__init__(buf, offset, parent)
-        logging.debug("MFTRECORD @ %s.", hex(offset))
 
         self.declare_field("dword", "magic")
         self.declare_field("word",  "usa_offset")
@@ -965,171 +953,6 @@ class MFTRecord(FixupBlock):
         Returns A binary string containing the MFT record slack.
         """
         return self._buf[self.offset():self.offset() + self.bytes_in_use()].tostring()
-
-
-class NTFSFile():
-    def __init__(self, options):
-        if type(options) == dict:
-            self.filename  = options["filename"]
-            self.filetype  = options["filetype"] or "mft"
-            self.offset    = options["offset"] or 0
-            self.clustersize = options["clustersize"] or 4096
-            self.mftoffset = False
-            self.prefix    = options["prefix"] or None
-            self.progress  = options["progress"]
-        else:
-            self.filename  = options.filename
-            self.filetype  = options.filetype
-            self.offset    = options.offset
-            self.clustersize = options.clustersize
-            self.mftoffset = False
-            self.prefix    = options.prefix
-            self.progress  = options.progress
-
-    # TODO calculate cluster size
-
-    def _calculate_mftoffset(self):
-        with open(self.filename, "rb") as f:
-            f.seek(self.offset)
-            f.seek(0x30, 1)  # relative
-            buf = f.read(8)
-            relmftoffset = struct.unpack_from("<Q", buf, 0)[0]
-            self.mftoffset = self.offset + relmftoffset * self.clustersize
-            logging.debug("MFT offset is %s", hex(self.mftoffset))
-
-    def record_generator(self, start_at=0):
-        """
-        @type start_at: int
-        @param start_at: the inode number to start at
-        @rtype generator of MFTRecord
-        """
-        if self.filetype == "indx":
-            return
-        if self.filetype == "mft":
-            size = os.path.getsize(self.filename)
-            is_redirected = os.fstat(0) != os.fstat(1)
-            should_progress = is_redirected and self.progress
-            with open(self.filename, "rb") as f:
-                record = True
-                count = start_at - 1
-                f.seek(start_at * 1024, 0)
-                while record:
-                    # TODO(wb): this really shouldn't be here
-                    if count % 100 == 0 and should_progress:
-                        n = (count * 1024 * 100) / float(size)
-                        sys.stderr.write("\rCompleted: %0.4f%%" % (n))
-                        sys.stderr.flush()
-                    count += 1
-                    buf = array.array("B", f.read(1024))
-                    if not buf:
-                        return
-                    try:
-                        record = MFTRecord(buf, 0, False, inode=count)
-                    except BinaryParser.OverrunBufferException:
-                        logging.debug("Failed to parse MFT record %s", str(count))
-                        continue
-                    logging.debug("Yielding record %d", count)
-                    yield record
-            if should_progress:
-                sys.stderr.write("\n")
-        if self.filetype == "image":
-            # TODO this overruns the MFT...
-            # TODO this doesnt account for a fragmented MFT
-            with open(self.filename, "rb") as f:
-                if not self.mftoffset:
-                    self._calculate_mftoffset()
-                f.seek(self.mftoffset + (start_at * 1024))
-                record = True
-                count = start_at - 1
-                while record:
-                    count += 1
-                    buf = array.array("B", f.read(1024))
-                    if not buf:
-                        return
-                    try:
-                        record = MFTRecord(buf, 0, False, inode=count)
-                    except BinaryParser.OverrunBufferException:
-                        logging.debug("Failed to parse MFT record %s", str(count))
-                        continue
-                    logging.debug("Yielding record %d", count)
-                    yield record
-
-    def mft_get_record_buf(self, number):
-        if self.filetype == "indx":
-            return array.array("B", "")
-        if self.filetype == "mft":
-            with open(self.filename, "rb") as f:
-                f.seek(number * 1024)
-                return array.array("B", f.read(1024))
-        if self.filetype == "image":
-            with open(self.filename, "rb") as f:
-                f.seek(number * 1024)
-                if not self.mftoffset:
-                    self._calculate_mftoffset()
-                f.seek(self.mftoffset)
-                f.seek(number * 1024, 1)
-                return array.array("B", f.read(1024))
-
-    def mft_get_record(self, number):
-        buf = self.mft_get_record_buf(number)
-        if buf == array.array("B", ""):
-            raise InvalidMFTRecordNumber(number)
-        return MFTRecord(buf, 0, False)
-
-    # memoization is key here.
-    @BinaryParser.memoize(100, keyfunc=lambda r, _:
-             str(r.magic()) + str(r.lsn()) + str(r.link_count()) + \
-             str(r.mft_record_number()) + str(r.flags()))
-    def mft_record_build_path(self, record, cycledetector=None):
-        if cycledetector is None:
-            cycledetector = {}
-        rec_num = record.mft_record_number() & 0xFFFFFFFFFFFF
-        if record.mft_record_number() & 0xFFFFFFFFFFFF == 0x0005:
-            if self.prefix:
-                return self.prefix
-            else:
-                return "\\."
-        fn = record.filename_information()
-        if not fn:
-            return "\\??"
-        parent_record_num = fn.mft_parent_reference() & 0xFFFFFFFFFFFF
-        parent_buf = self.mft_get_record_buf(parent_record_num)
-        if parent_buf == array.array("B", ""):
-            return "\\??\\" + fn.filename()
-        parent = MFTRecord(parent_buf, 0, False)
-        if parent.sequence_number() != fn.mft_parent_reference() >> 48:
-            return "\\$OrphanFiles\\" + fn.filename()
-        if rec_num in cycledetector:
-            logging.debug("Cycle detected")
-            if self.prefix:
-                return self.prefix + "\\<CYCLE>"
-            else:
-                return "\\<CYCLE>"
-        cycledetector[rec_num] = True
-        return self.mft_record_build_path(parent, cycledetector) + "\\" + fn.filename()
-
-    def mft_get_record_by_path(self, path):
-        # TODO could optimize here by trying to use INDX buffers
-        # and actually walk through the FS
-        count = -1
-        for record in self.record_generator():
-            count += 1
-            if record.magic() != 0x454C4946:
-                continue
-            if not record.is_active():
-                continue
-            record_path = self.mft_record_build_path(record, {})
-            if record_path.lower() != path.lower():
-                continue
-            return record
-        return False
-
-    def read(self, offset, length):
-        if self.filetype == "image":
-            with open(self.filename, "rb") as f:
-                f.seek(offset)
-                return array.array("B", f.read(length))
-        return array.array("B", "")
 
 
 class InvalidAttributeException(INDXException):
@@ -1224,7 +1047,7 @@ class MFTEnumerator(object):
             raise BinaryParser.OverrunBufferException(end, len(self._buf))
 
         buf = self._buf[start:end]
-        return array.array("B", buf)
+        return buf
 
     def get_record(self, record_num):
         """
